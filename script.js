81090596d569fda70b1e58a1475939ea571fcbbf
IsabeliAlexandre <isabelialexandredasilvaaraujo@gmail.com>
2026-03-20 09:21:50 -0300

    Atv5

--- script.js
+++ script.js
@@ -3,25 +3,33 @@ let btnAdicionar = document.getElementById("btnAdicionar");
 let listaTarefas = document.getElementById("listaTarefas");
 let mensagem = document.getElementById("mensagem");
 
-btnAdicionar.addEventListener("click", () => {
+btnAdicionar.addEventListener("click", () =>  {
 
     let mensagemDeTexto = inputTarefa.value;
-
-    if (mensagemDeTexto === "") {
+    if (mensagemDeTexto == '') {
 
         mensagem.innerHTML =
-        '<div class="alert alert-danger" role="alert">Adicione uma tarefa🤦</div>';
+        '<div class="alert alert-danger">Adicione uma tarefa🤦</div>'; // classe bootstrap para mensagem de problema
 
     } else {
+
         let lista = document.createElement("li");
-        lista.className = "list-group-item";
+        lista.className = "list-group-item d-flex justify-content-between align-items-center"; 
         lista.innerText = mensagemDeTexto;
 
+        let btnRemover = document.createElement("button");
+        btnRemover.innerText = "Remover Tarefa";
+        btnRemover.className = "btn btn-danger"; // classe de botão vermleho 
+
+        btnRemover.addEventListener("click", ()=> {
+            lista.remove()
+        }),
+
+        lista.appendChild(btnRemover);
         listaTarefas.appendChild(lista);
         inputTarefa.value = "";
 
-        mensagem.innerHTML =
-        '<div class="alert alert-success" role="alert">Tarefa adicionada!</div>';
+        mensagem.innerHTML = '<div class="alert alert-success">Tarefa adicionada!</div>'; // classe bootstrap para mensagem de sucesso
     }
 
 });
