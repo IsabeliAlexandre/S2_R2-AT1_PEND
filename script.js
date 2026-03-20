43c78d46ae7072c8c52782c857207790a7fc03fd
IsabeliAlexandre <isabelialexandredasilvaaraujo@gmail.com>
2026-03-20 10:05:17 -0300

    Atv6

--- script.js
+++ script.js
@@ -3,33 +3,43 @@ let btnAdicionar = document.getElementById("btnAdicionar");
 let listaTarefas = document.getElementById("listaTarefas");
 let mensagem = document.getElementById("mensagem");
 
-btnAdicionar.addEventListener("click", () =>  {
+btnAdicionar.addEventListener("click", () => {
 
     let mensagemDeTexto = inputTarefa.value;
-    if (mensagemDeTexto == '') {
 
-        mensagem.innerHTML =
-        '<div class="alert alert-danger">Adicione uma tarefa🤦</div>'; // classe bootstrap para mensagem de problema
+    if (mensagemDeTexto === "") {
+        mensagem.innerHTML = '<div class="alert alert-danger">Digite uma tarefa 🤦</div>';
 
     } else {
 
         let lista = document.createElement("li");
-        lista.className = "list-group-item d-flex justify-content-between align-items-center"; 
+        lista.className = "list-group-item d-flex justify-content-between align-items-center";
         lista.innerText = mensagemDeTexto;
+
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox"; // cria o checkbox para clicar
+
+        checkbox.addEventListener = () => {
+            if (checkbox.checked) {
+                lista.style.textDecoration = "line-through";
+            } else {
+                lista.style.textDecoration = "none";
+            }
+        };
+
+        lista.appendChild(checkbox);
 
         let btnRemover = document.createElement("button");
         btnRemover.innerText = "Remover Tarefa";
-        btnRemover.className = "btn btn-danger"; // classe de botão vermleho 
+        btnRemover.className = "btn btn-danger"; // classe de botão vermelho 
 
-        btnRemover.addEventListener("click", ()=> {
+        btnRemover.addEventListener("click", () => {
             lista.remove()
         }),
 
-        lista.appendChild(btnRemover);
+            lista.appendChild(btnRemover);
         listaTarefas.appendChild(lista);
         inputTarefa.value = "";
-
         mensagem.innerHTML = '<div class="alert alert-success">Tarefa adicionada!</div>'; // classe bootstrap para mensagem de sucesso
     }
-
 });
